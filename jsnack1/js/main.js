ae0fefc1b594caebfa5c4bf6e3ef74d6b995b9aa
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-27 14:30:32 +0100

    Added JSnack1 DOM

--- jsnack1/js/main.js
+++ jsnack1/js/main.js
@@ -1,13 +1,26 @@
-// Chiedo di inserire un numero
-const userNumber = parseInt(prompt('Inserisci un numero'));
+// Input Var
+const sendButton = document.getElementById('send_button');
 
-// SE il numero diviso per due da resto 0
-if (userNumber % 2 == 0) {
-    // Stampo il numero
-    console.log(userNumber);
-}
-// ALTRIMENTI
-else {
-    // Stampo il numero + 1;
-    console.log(userNumber + 1);
-}
+// Output Var
+const resultNumber = document.getElementById('result_output');
+
+// Aggiungo un Event Listener al bottone
+sendButton.addEventListener(
+    'click',
+
+    function () {
+        // Chiedo di inserire un numero
+        const userNumber = parseInt(document.getElementById('user_number').value);
+
+        // SE il numero diviso per due da resto 0
+        if (userNumber % 2 == 0) {
+            // Stampo il numero
+            resultNumber.innerHTML = `Il numero è: ${userNumber}`
+        }
+        // ALTRIMENTI
+        else {
+            // Stampo il numero + 1;
+            resultNumber.innerHTML = `Il numero è: ${userNumber + 1}`
+        }
+    }
+)
